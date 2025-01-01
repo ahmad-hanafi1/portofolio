dac162374105d74a66c94ef05947f85d71ec2efe
ahmad-hanafi1 <ahmadhanafi235@gmail.com>
2025-01-01 19:59:07 +0100

    working on page components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motions";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        className="=w-full p-1 rounded-[20px] green-pink-gradient shadow-card"
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      >
+        <div
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[260px] flex justify-evenly items-center flex-col"
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        className="mt-4 text-secondary leading-[30px] text-[17px] max-w-3xl"
+        variants={fadeIn("", "", 0.1, 1)}
+      >
+        I&apos;m an Innovative and detail-oriented software developer with 2
+        years of experience building responsive web applications. Proficient in
+        web development with experience in both front-end and back-end
+        development, ensuring top-quality responsive websites with a focus on
+        performance enhancement and cross-browser compatibility. Equipped with a
+        strong understanding of user interface design principles and modern web
+        development practices. Adept at using agile methodologies and
+        collaborating with cross-functional teams to deliver appropriate and
+        high-quality software solutions.
+      </motion.p>
 
-export default About
+      <div className="mt-20 flex flex-wrap gap-10 ">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={1.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        // eslint-disable-next-line react/no-unknown-property
+        object={computer.scene}
+        // eslint-disable-next-line react/no-unknown-property
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        // eslint-disable-next-line react/no-unknown-property
+        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 0.75}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  console.log(isMobile);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
